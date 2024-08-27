caedae3cc3b9d8ad5e2a5af4974480779e93acea
Kira <90696175+KiraPixel@users.noreply.github.com>
2024-08-28 00:01:17 +0300

    first push

---
+++ models.py
@@ -0,0 +1,89 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, JSON, ForeignKey, Float, Boolean, Index
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+import config
+
+Base = declarative_base()
+
+
+class Transport(Base):
+    __tablename__ = 'transport'
+    id = Column(Integer, primary_key=True)
+    storage_id = Column(Integer, ForeignKey('storage.ID'), nullable=False)
+    model_id = Column(Integer, ForeignKey('transport_model.id'), nullable=False)
+    uNumber = Column(Text)
+    vin = Column(Text)
+    equipment = Column(JSON)
+    x = Column(Float)
+    y = Column(Float)
+
+    def __repr__(self):
+        return f'<Transport {self.uNumber}>'
+
+
+class Alert(Base):
+    __tablename__ = 'alert'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(Integer, nullable=False, default=0)
+    uNumber = Column(Text, nullable=False)
+    type = Column(Text, nullable=False)
+    data = Column(Text, nullable=False)
+    status = Column(Integer, nullable=True, default=0)
+
+    def __repr__(self):
+        return f'<Alert {self.uNumber}, Type: {self.type}>'
+
+
+class CashCesar(Base):
+    __tablename__ = 'cash_cesar'
+    unit_id = Column(Integer, primary_key=True)
+    object_name = Column(Text, nullable=False)
+    pin = Column(Integer, default=0)
+    vin = Column(Text, nullable=False)
+    last_time = Column(Integer, default=0)
+    pos_x = Column(Float, default=0.0)
+    pos_y = Column(Float, default=0.0)
+    created_at = Column(Integer, default=0)
+    device_type = Column(Text, nullable=False)
+    linked = Column(Boolean, nullable=True, default=False)
+
+    __table_args__ = (
+        Index('idx_cash_cesar_object_name', 'object_name'),
+    )
+
+
+class CashWialon(Base):
+    __tablename__ = 'cash_wialon'
+    id = Column(Integer, primary_key=True)
+    uid = Column(Integer, nullable=False, default=0)
+    nm = Column(Text, nullable=False)
+    pos_x = Column(Float, default=0.0)
+    pos_y = Column(Float, default=0.0)
+    last_time = Column(Integer, default=0)
+    last_pos_time = Column(Integer, default=0)
+    linked = Column(Boolean, nullable=True, default=False)
+
+    __table_args__ = (
+        Index('idx_cash_wialon_nm', 'nm'),
+    )
+
+# Индекс для поля uNumber в Transport
+Index('idx_transport_unumber', Transport.uNumber)
+
+
+def get_engine():
+    """Возвращает объект engine для базы данных"""
+    return create_engine(config.SQLALCHEMY_DATABASE_URL)
+
+
+def create_db():
+    """Создает базу данных и таблицы"""
+    engine = get_engine()
+    Base.metadata.create_all(engine)
+    return engine
+
+
+def create_session(engine):
+    """Создает и возвращает сессию"""
+    Session = sessionmaker(bind=engine)
+    return Session()
---
+++ run.py
@@ -0,0 +1,141 @@
+import sys
+import time
+
+import schedule
+from tqdm import tqdm
+from models import create_session, get_engine, Transport, Alert, CashWialon
+from location_module import calculate_distance
+from system_status_manager import get_status as get_db_status
+
+
+# Создаем engine и сессию
+engine = get_engine()
+session = create_session(engine)
+
+
+def create_alert(uNumber, type, data):
+    """Создает новый алерт"""
+    new_alert = Alert(
+        date=int(time.time()),  # текущая дата в формате UNIX
+        uNumber=uNumber,
+        type=type,
+        data=data
+    )
+    session.add(new_alert)
+    session.commit()
+
+
+def alert_update(uNumber, type, data):
+    """Обновляет data у существующего алерта"""
+    alert = session.query(Alert).filter_by(uNumber=uNumber, type=type, status=0).first()
+    if alert:
+        alert.data = data
+        session.commit()
+
+
+def close_alert(uNumber, type):
+    """Закрывает алерт, изменяя его статус на 1"""
+    alert = session.query(Alert).filter_by(uNumber=uNumber, type=type, status=0).first()
+    if alert:
+        alert.status = 1
+        session.commit()
+
+
+def search_alert(uNumber, type, data=None):
+    """Ищет алерт по uNumber, type и data"""
+    query = session.query(Alert).filter_by(uNumber=uNumber, type=type)
+    if data:
+        query = query.filter(Alert.data.like(f"%{data}%"))
+    return query.first()
+
+
+def process_wialon(uNumber, transport_cord):
+    """отрабатываем часть wialon"""
+    wialon = session.query(CashWialon).filter(CashWialon.nm.like(f"%{uNumber}%")).first()
+    equipment_alert = search_alert(uNumber, 'no_equipment', 'Wialon')
+    if not wialon:
+        if not equipment_alert:
+            create_alert(uNumber, 'no_equipment', 'Wialon')
+            return
+        return
+
+    # Сохранение данных wialon
+    pos_x, pos_y, last_time = wialon.pos_x, wialon.pos_y, wialon.last_time
+    wialon_cords = pos_x, pos_y
+
+    # Чекаем есть ли алерты
+    gps_alert = search_alert(uNumber, 'gps', 'Wialon')
+    not_work_alert = search_alert(uNumber, 'not_work', 'Wialon')
+    danger_alert = search_alert(uNumber, 'distance')
+
+    # накидываем тригеры
+    trigger_pos = False
+    trigger_not_work = False
+    #накидываем тригеры
+    if pos_x == 0 or pos_y == 0:
+        trigger_pos = True
+    if time.time() - last_time > 48 * 3600:
+        trigger_not_work = True
+
+    # создаем или закрываем алерт по позиции
+    if trigger_pos and not gps_alert:
+        create_alert(uNumber, 'gps', 'Wialon')
+    elif not trigger_pos and gps_alert:
+        close_alert(uNumber, 'gps')
+
+    # создаем или закрываем алерт по работе оборудования
+    if trigger_not_work and not not_work_alert:
+        create_alert(uNumber, 'not_work', 'Wialon')
+    elif not trigger_not_work and not_work_alert:
+        close_alert(uNumber, 'not_work')
+
+    # закрываем алерт на дистанцию, если, что-то не работает
+    if trigger_pos or trigger_not_work:
+        close_alert(uNumber, 'distance')
+        return
+
+    # блок дистанции
+    danger_distance = 5  # дистанция в км, которую мы считаем опасной
+    if transport_cord is not None:
+        distance = calculate_distance(transport_cord, wialon_cords)
+        if distance >= danger_distance:
+            if not danger_alert:
+                create_alert(uNumber, 'distance', distance)
+            else:
+                alert_update(uNumber, 'distance', distance)  # Обновление дистанции в алерте
+        else:
+            if danger_alert:
+                close_alert(uNumber, 'danger')
+
+
+def process_transports():
+    """Основная функция для обработки данных транспортных средств"""
+    # Получаем все транспортные средства
+    transports = session.query(Transport).all()
+
+    print("Начало обработки:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    start_time = time.time()
+
+    for transport in tqdm(transports, desc=f"Обработка транспортных средств", unit="тс"):
+        while get_db_status('db') == 1:
+            time.sleep(1)
+        uNumber = transport.uNumber
+        transport_cord = None
+        if transport.x != 0 and transport.y != 0:
+            transport_cord = transport.y, transport.x  # переворачиваем корды, ибо это баг виалона
+
+        process_wialon(uNumber, transport_cord)
+
+    end_time = time.time()
+    print("\nОбработка завершена:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    print(f"Время обработки: {end_time - start_time:.2f} секунд")
+
+
+# Расписание задач
+schedule.every(10).minutes.do(process_transports)  # Выполнять update_db() каждую минуту
+
+if __name__ == "__main__":
+    print("Запуск планировщика задач...")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)  # Задержка, чтобы не перегружать процессор
